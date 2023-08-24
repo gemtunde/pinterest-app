dbcb4e454171e4096ea4b53f96bd84b834747605
gemtunde <gemtunde@gmail.com>
2023-08-24 17:40:10 +0100

    post form to db

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import UploadImage from "@/app/components/UploadImage";
 import UserTag from "@/app/components/UserTag";
 import { useSession } from "next-auth/react";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import app from "../Shared/firebaseConfig";
+import { getFirestore, addDoc, doc, setDoc } from "firebase/firestore";
 
 const Form = () => {
   const { data: session } = useSession();
@@ -12,12 +15,52 @@ const Form = () => {
     description: "",
     link: "",
     file: "",
+    userName: "",
+    userImage: "",
+    email: "",
   });
 
   const onSave = (e) => {
     // e.preventDefault();
     console.log(formData);
+    uploadFile();
   };
+
+  //storage files
+  const storage = getStorage(app);
+
+  //initialize firstore
+  const db = getFirestore(app);
+
+  //use timestamp for unique id
+  const postId = Date.now().toString();
+
+  const uploadFile = () => {
+    const storageRef = ref(storage, `pinterest/${formData.file}`);
+    uploadBytes(storageRef, formData.file)
+      .then((snapshot) => {
+        console.log("files uploaded");
+      })
+      .then((res) => {
+        getDownloadURL(storageRef).then(async (url) => {
+          // console.log("url download", url);
+          const postData = {
+            ...formData,
+            file: url,
+            userName: session?.user?.name,
+            email: session?.user?.email,
+            userImage: session?.user?.image,
+          };
+          console.log("postdata", postData);
+          await setDoc(doc(db, "pinterest-post", postId), postData).then(
+            (resp) => {
+              console.log("data saved");
+            }
+          );
+        });
+      });
+  };
+
   return (
     <div className="bg-white p-16 rounded-2xl">
       <div className="flex justify-end mb-6">
